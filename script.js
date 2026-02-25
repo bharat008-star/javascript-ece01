738915cf3be57bc4d54fc5bee7142122fcd31160
bharat008-star <https://github.com/bharat008-star/HTML-ECE1.git>
2026-02-25 10:12:51 +0530

    java operators

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ console.log(typeof(num));
 num = 10 ;
 console.log(num);
 console.log(typeof(num));
+const sym1 = Symbol(4);
+console.log(sym1);
+const sym2 = Symbol(4);
+console.log(sym2);
+if(sym1 == sym2){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
