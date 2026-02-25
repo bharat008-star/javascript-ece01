a46f344a1cf3ccbd5829bd79f891e95b3527b0db
bharat008-star <https://github.com/bharat008-star/HTML-ECE1.git>
2026-02-25 09:50:47 +0530

    java types

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("hello world");
+var num  ;
+console.log(num);
+console.log(typeof(num));
+num = false;
+console.log(num);
+console.log(typeof(num));
+num = "aditya sarin";
+console.log(num);
+console.log(typeof(num));
+num= null;
+console.log(num);
+console.log(typeof(num));
+num = 10 ;
+console.log(num);
+console.log(typeof(num));
