0305f7cf82e62eee355128b6fc2571c698528d47
bharat008-star <https://github.com/bharat008-star/HTML-ECE1.git>
2026-03-11 09:20:10 +0530

    for

--- loop.js
+++ loop.js
@@ -1,6 +1,15 @@
-let count;
-document.write("starting loop <br>");
-for(count = 0; count < 10; count++){
-    document.write( "current count" + count + "<br>");
+// let count;
+// document.write("starting loop <br>");
+// for(count = 0; count < 10; count++){
+//     document.write( "current count" + count + "<br>");
+// }
+// document.write("loop ended <br>")
+
+const person={
+    name:"ajay",
+    age:25,
+    city:"delhi"
+};
+for(let x in person){
+    document.write(x + " : " + person[x] + "<br>");
 }
-document.write("loop ended <br>")
